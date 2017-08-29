fe54a44456979b0701c3ff4bc0d44726e4f5b830
griffio <griffio@users.noreply.github.com>
2017-08-29 21:01:11 +0100

    Kotlin 1.1.4-2

--- src/main/kotlin/griffio/Screen.kt
+++ src/main/kotlin/griffio/Screen.kt
@@ -3,46 +3,48 @@ package griffio
 import com.google.common.collect.ArrayTable
 import com.google.common.collect.Table
 import com.google.common.collect.Tables
-import com.googlecode.lanterna.TerminalFacade
+import com.googlecode.lanterna.SGR
+import com.googlecode.lanterna.TextCharacter
+import com.googlecode.lanterna.TextColor.ANSI
 import com.googlecode.lanterna.screen.Screen
-import com.googlecode.lanterna.screen.ScreenCharacterStyle
-import com.googlecode.lanterna.screen.ScreenWriter
-import com.googlecode.lanterna.terminal.Terminal
+import com.googlecode.lanterna.screen.TerminalScreen
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory
+import com.googlecode.lanterna.terminal.TerminalResizeListener
 
-sealed class Tile(val char: String, val color: Terminal.Color) {
-    class Floor() : Tile("\u00B7", Terminal.Color.YELLOW)
-    class Wall() : Tile("\u2593", Terminal.Color.CYAN)
-    class Bounds() : Tile("x", Terminal.Color.BLACK)
+sealed class Tile(val char: Char, val color: ANSI) {
+    class Floor() : Tile('\u00B7', ANSI.YELLOW)
+    class Wall() : Tile('\u2593', ANSI.CYAN)
+    class Bounds() : Tile('x', ANSI.BLACK)
 }
 
-class WorldBuilder(val height: Int, val width: Int) {
+class WorldBuilder(private val height: Int, private val width: Int) {
 
-    var worldTiles: Table<Int, Int, Tile> = randomWorld(ArrayTable.create<Int, Int, Tile>(0..height - 1, 0..width - 1))
+    var worldTiles: Table<Int, Int, Tile> = randomWorld(ArrayTable.create<Int, Int, Tile>(0 until height, 0 until width))
 
-    val smoothing = 1..8
+    private val smoothing = 1..8
 
-    val offset = -1..1
+    private val offset = -1..1
 
     fun build(): WorldView {
         return WorldView(smooth(worldTiles))
     }
 
-    fun randomWorld(tiles: Table<Int, Int, Tile>): Table<Int, Int, Tile> {
-        return Tables.transformValues(tiles, { tile -> if (Math.random() < 0.5) Tile.Floor() else Tile.Wall() })
+    private fun randomWorld(tiles: Table<Int, Int, Tile>): Table<Int, Int, Tile> {
+        return Tables.transformValues(tiles, { _ -> if (Math.random() < 0.5) Tile.Floor() else Tile.Wall() })
     }
 
     fun smooth(tiles: Table<Int, Int, Tile>): Table<Int, Int, Tile> {
 
         var result = tiles
 
-        val tiles2 = ArrayTable.create<Int, Int, Tile>(0..height - 1, 0..width - 1)
+        val tiles2 = ArrayTable.create<Int, Int, Tile>(0 until height, 0 until width)
 
         smoothing.forEach {
             for (tileCell in result.cellSet()) {
                 val col = tileCell.columnKey
                 val row = tileCell.rowKey
-                var floors: Int = 0
-                var walls: Int = 0
+                var floors = 0
+                var walls = 0
                 offset.forEach { ox ->
                     (offset.forEach { oy ->
                         when (result.get(row?.plus(oy), col?.plus(ox))) {
@@ -63,76 +65,71 @@ class WorldBuilder(val height: Int, val width: Int) {
 class WorldView(val tiles: Table<Int, Int, Tile>) {
 }
 
-class DefaultWriter(val screen: Screen) : ScreenWriter(screen) {
-    init {
-        backgroundColor = Terminal.Color.DEFAULT
-    }
+class DefaultWriter(val screen: Screen) {
 
-    fun centerString(text: String, vararg styles: ScreenCharacterStyle) {
+    fun centerText(text: String, vararg modifiers: SGR) {
         val x = (screen.terminalSize.columns - text.length) / 2
         val y = screen.terminalSize.rows / 2
-        super.drawString(x, y, text, *styles)
+        val graphics = screen.newTextGraphics()
+        graphics.enableModifiers(*modifiers);
+        graphics.putString(x, y, text)
+    }
+
+    fun drawText(column: Int, row: Int, text: String, vararg modifiers: SGR) {
+
     }
 }
 
-class WorldWriter(val screen: Screen) : ScreenWriter(screen) {
-
-    init {
-        backgroundColor = Terminal.Color.DEFAULT
-    }
+class WorldWriter(private val screen: Screen) {
 
     fun drawWorld() {
+
+        val text = screen.newTextGraphics()
 
         val world = WorldBuilder(screen.terminalSize.rows, screen.terminalSize.columns).build()
 
         val tiles = world.tiles.cellSet()
 
         for (tile in tiles) {
-            screen.putString(tile.columnKey!!, tile.rowKey!!, tile.value?.char, tile.value?.color, Terminal.Color.DEFAULT)
+            text.setCharacter(tile.columnKey!!, tile.rowKey!!, TextCharacter(tile.value!!.char, tile.value!!.color, ANSI.DEFAULT))
         }
     }
-
-    fun centerString(text: String, vararg styles: ScreenCharacterStyle) {
-        val x = (screen.terminalSize.columns - text.length) / 2
-        val y = screen.terminalSize.rows / 2
-        super.drawString(x, y, text, *styles)
-    }
-
 }
 
 fun main(args: Array<String>) {
-    val term = TerminalFacade.createTerminal()
-    val screen = Screen(term)
+
+    val term = DefaultTerminalFactory().createTerminal()
+    val screen = TerminalScreen(term)
     val writerDefault = DefaultWriter(screen)
     val worldWriter = WorldWriter(screen)
 
-    screen.setPaddingCharacter(' ', Terminal.Color.DEFAULT, Terminal.Color.DEFAULT)
-    screen.startScreen()
-
-    worldWriter.drawWorld()
-
-    draw(writerDefault)
-    screen.refresh()
-
-    var key = term.readInput()
-
-    while (key == null) {
-        if (screen.resizePending()) {
-            screen.clear()
-            worldWriter.drawWorld()
-            draw(writerDefault)
-            screen.refresh()
-        }
-
-        Thread.sleep(25)
-        key = term.readInput()
+    fun updateWorld() {
+        screen.clear()
+        worldWriter.drawWorld()
+        draw(writerDefault)
+        screen.refresh()
     }
 
+    val resized = TerminalResizeListener { _, _ ->
+        updateWorld()
+    }
+
+    term.addResizeListener(resized)
+
+    screen.startScreen()
+
+    updateWorld()
+
+    do {
+        Thread.sleep(25)
+    } while (term.pollInput() == null)
+
     screen.stopScreen()
+
     System.exit(0)
 }
 
 fun draw(writer: DefaultWriter) {
-    writer.centerString("Press any key to quit...", ScreenCharacterStyle.Blinking)
-    writer.drawString(0, writer.screen.terminalSize.rows - 1, "griffio 2016", ScreenCharacterStyle.Underline)
+    writer.centerText("Press any key to quit...", SGR.BLINK)
+    writer.drawText(0, writer.screen.terminalSize.rows - 1, "griffio 2017", SGR.UNDERLINE)
 }
--- src/test/kotlin/griffio/WorldTest.kt
+++ src/test/kotlin/griffio/WorldTest.kt
@@ -2,16 +2,15 @@ package griffio
 
 import org.junit.Assert
 import org.junit.Test
-import java.util.*
+import java.util.StringJoiner
 
 class WorldTest {
 
     @Test
     fun randomWorldView() {
         val world = WorldBuilder(80, 40).build()
-        println("world = ${world}")
+        println("world = $world")
     }
-
     /**
      * [21 29] [22 29] [23 29]
      * [21 30] [22 30] [23 30]
@@ -23,7 +22,7 @@ class WorldTest {
         Assert.assertEquals("(21, 29) (21, 30) (21, 31) (22, 29) (22, 30) (22, 31) (23, 29) (23, 30) (23, 31)", actual)
     }
 
-    fun blockCords(x: Int, y: Int): String {
+    private fun blockCords(x: Int, y: Int): String {
         return join {
             (x - 1..x + 1).forEach { xn ->
                 (y - 1..y + 1).forEach { yn ->
@@ -33,9 +32,28 @@ class WorldTest {
         }
     }
 
-    fun join(build: StringJoiner.() -> Unit): String {
-        var joiner = StringJoiner(" ")
+    private fun join(build: StringJoiner.() -> Unit): String {
+        val joiner = StringJoiner(" ")
         joiner.build()
         return joiner.toString()
+    }
+
+    @Test
+    fun x() {
+        val worldXY = WorldXY(10, 6)
+        (0 until worldXY.sizeX).forEachIndexed { idx, _ ->
+            worldXY.setTileType(idx, 0, '┃')
+            worldXY.setTileType(idx, worldXY.sizeY-1, '┃')
+        }
+
+        (0 until worldXY.sizeY).forEachIndexed { idx, _ ->
+            worldXY.setTileType(0, idx, '━')
+            worldXY.setTileType(worldXY.sizeX-1, idx, '━')
+        }
+        worldXY.setTileType(0, 0, '┏')
+        worldXY.setTileType(0, worldXY.sizeY-1, '┓')
+        worldXY.setTileType(worldXY.sizeX-1, 0, '┗')
+        worldXY.setTileType(worldXY.sizeX-1, worldXY.sizeY-1, '┛')
+        println(worldXY)
     }
 }
